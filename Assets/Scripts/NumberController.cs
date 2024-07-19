4c480768ee2c6a83cb0a6e1a28b72e11b0d93a6b
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-07-20 00:57:11 +0900

    [feat] 버튼 온클릭 이벤트 생성
    - 호버하면 색이 바뀌게

---
+++ Assets/Scripts/NumberController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberController : MonoBehaviour
+{
+    private int plus;
+    private int minus;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
