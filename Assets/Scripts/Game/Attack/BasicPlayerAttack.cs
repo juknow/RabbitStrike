970e78da6da81cee87734f5b9583ccac145a7154
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-07-21 22:35:29 +0900

    [feat] 기본 플레이어 공격 모션 구현
    - 공격 애니메이션 구현
    - 공격 모션 실행 스크립트 추가

---
+++ Assets/Scripts/Game/Attack/BasicPlayerAttack.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayerAttack : MonoBehaviour
+{
+    private Animator animator;
+    public bool isInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInRange = true;
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            animator.SetBool("isInRange", true);
+        }
+        else
+        {
+            animator.SetBool("isInRange", false);
+        }
+    }
+
+}
+
