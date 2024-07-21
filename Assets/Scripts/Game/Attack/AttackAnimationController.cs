46e592da6868f676c10770862348b98dbaad8550
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-07-21 23:02:52 +0900

    [feat] 캐릭터 공격 모션 전부 추가

--- Assets/Scripts/Game/Attack/AttackAnimationController.cs
+++ Assets/Scripts/Game/Attack/AttackAnimationController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicPlayerAttack : MonoBehaviour
+public class AttackAnimationController : MonoBehaviour
 {
     private Animator animator;
     public bool isInRange;
