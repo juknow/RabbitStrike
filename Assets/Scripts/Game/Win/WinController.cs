88a019ad4df5101119e1b80475cdf84b60ceffbd
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-07-22 15:44:41 +0900

    [feat] 승리 , 패배 판정 추가

---
+++ Assets/Scripts/Game/Win/WinController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        // Check for objects with Enemy tag
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        // Determine the winner based on the presence of Player and Enemy objects
+        if (players.Length == 0 && enemies.Length == 0)
+        {
+            Debug.Log("Draw! No Players or Enemies left.");
+            SceneManager.LoadScene("Stage");
+        }
+        else if (players.Length == 0)
+        {
+            Debug.Log("Enemy Wins! No Players left.");
+            SceneManager.LoadScene("Stage");
+        }
+        else if (enemies.Length == 0)
+        {
+            Debug.Log("Player Wins! No Enemies left.");
+            SceneManager.LoadScene("Stage");
+        }
+
+    }
+}
